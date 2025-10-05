63e8957cb0a0d782a6419f6fa4b09bfa7e386248
erynder-z <stfn.bgr@gmail.com>
2025-10-05 15:55:15 +0200

    Refactor config load

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,20 +2,24 @@
 // so we use adapter-static with a fallback to index.html to put the site in SPA mode
 // See: https://svelte.dev/docs/kit/single-page-apps
 // See: https://v2.tauri.app/start/frontend/sveltekit/ for more info
-import { invoke } from "@tauri-apps/api/core";
 import { appConfig } from "$lib/store";
 import type { AppConfig } from "$lib/store";
 import { setLocale } from "$lib/i18n";
+import { browser } from "$app/environment";
+import type { LoadEvent } from "@sveltejs/kit";
 
 export const ssr = false;
 
-export async function load() {
-  try {
-    const configStr: string = await invoke("read_config_command");
-    const config: AppConfig = JSON.parse(configStr);
-    appConfig.set(config);
-    setLocale(config.language);
-  } catch (error) {
-    console.error("Failed to load config:", error);
+export async function load({ fetch }: LoadEvent) {
+  if (browser) {
+    try {
+      const { invoke } = await import("@tauri-apps/api/core");
+      const configStr: string = await invoke("read_config_command");
+      const config: AppConfig = JSON.parse(configStr);
+      appConfig.set(config);
+      setLocale(config.language);
+    } catch (error) {
+      console.error("Failed to load config:", error);
+    }
   }
 }
