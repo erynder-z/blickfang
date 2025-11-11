5f33b034ee30400df383f6e9d5118538b4a9807e
erynder-z <stfn.bgr@gmail.com>
2025-11-11 21:47:12 +0100

    Show placeholder in sidebar when no image is loaded

--- src/lib/components/InfoSidebar.svelte
+++ src/lib/components/InfoSidebar.svelte
@@ -1,18 +1,23 @@
 <script lang="ts">
-  import { isExifSidebarVisible } from "$lib/stores/appState";
+  import { isInfoSidebarVisible, imageFormat } from "$lib/stores/appState";
   import ExifDisplay from "./ExifDisplay.svelte";
   import ImageDetails from "./ImageDetails.svelte";
+  import InfoSidebarPlaceholder from "./InfoSidebarPlaceholder.svelte";
 </script>
 
-<div class="exif-sidebar-overlay" class:visible={$isExifSidebarVisible}>
+<div class="info-sidebar-overlay" class:visible={$isInfoSidebarVisible}>
   <div class="sidebar-content">
-    <ImageDetails />
-    <ExifDisplay />
+    {#if $imageFormat}
+      <ImageDetails />
+      <ExifDisplay />
+    {:else}
+      <InfoSidebarPlaceholder />
+    {/if}
   </div>
 </div>
 
 <style>
-  .exif-sidebar-overlay {
+  .info-sidebar-overlay {
     position: absolute;
     top: 0;
     right: 0;
@@ -21,12 +26,12 @@
     max-width: 30ch;
     background-color: var(--color-background);
     transform: translateX(100%);
-    transition: transform 0.3s ease-in-out;
+    transition: transform 0.2s ease-in-out;
     z-index: 20;
     border-left: 0.2rem solid var(--color-outline);
   }
 
-  .exif-sidebar-overlay.visible {
+  .info-sidebar-overlay.visible {
     transform: translateX(0);
   }
 
---
+++ src/lib/components/InfoSidebarPlaceholder.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { t } from "$lib/utils/i18n";
+</script>
+
+<div class="placeholder-container">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="currentColor"
+    ><path
+      d="m840-234-80-80v-446H314l-80-80h526q33 0 56.5 23.5T840-760v526ZM792-56l-64-64H200q-33 0-56.5-23.5T120-200v-528l-64-64 56-56 736 736-56 56ZM240-280l120-160 90 120 33-44-283-283v447h447l-80-80H240Zm297-257ZM424-424Z"
+    /></svg
+  >
+  <p>{$t["placeholder.no-image"]}</p>
+</div>
+
+<style>
+  .placeholder-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    gap: 1rem;
+    color: var(--color-text-secondary);
+    padding: 1rem;
+    text-align: center;
+  }
+
+  svg {
+    width: 4rem;
+    height: 4rem;
+    fill: var(--color-text-secondary);
+  }
+</style>
